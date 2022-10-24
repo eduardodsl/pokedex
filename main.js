f3fa26a00fad672b3a94e52256b7222e223c9b68
Eduardo Augusto <eduardodsl@gmail.com>
2022-10-24 19:14:14 +0000

    adding lazy loading scroll, main class refactoring

--- main.js
+++ main.js
@@ -7,23 +7,38 @@ class Main {
 
     constructor(){
         this.pokemonService = new PokemonService();
+        this.listTemplate = new PokemonListTemplate();
+        this.detailsTemplate = new PokemonDetailsTemplate({ show: false });
+        this.offset = 0;
+        this.limit = 20;
+        this.loading = false;
     }
 
-    /**
-     * Adds application's template base events
-     * @param {any} listTemplate 
-     * @param {any} detailsTemplate 
-     */
-    addEvents(listTemplate, detailsTemplate){
-        listTemplate.onMount((data) => {
+    addOffset(){
+        return this.offset += 20;
+    }
+
+    addEvents(){
+        this.listTemplate.onMount((data) => {
             document.querySelectorAll("#pokemon-list li.pokemon-item").forEach((el) => {
                 el.addEventListener("click", (ev) => {
                     ev.stopPropagation();
                     this.pokemonService.selectPokemon(ev.currentTarget.dataset.pkmid);
-                    detailsTemplate.data.pokemon = this.pokemonService.getSelectedPokemon();
-                    detailsTemplate.data.show = true;
-                    detailsTemplate.update();
+                    this.detailsTemplate.data.pokemon = this.pokemonService.getSelectedPokemon();
+                    this.detailsTemplate.data.show = true;
+                    this.detailsTemplate.update();
                 });
+            });
+            document.querySelector("#search").addEventListener("scroll", async (e) => {
+                const scrollStatus = new ScrollStatus(e.currentTarget);
+                if(scrollStatus.isBottom() && !this.loading && !this.pokemonService.atLimit()){
+                    this.loading = true;
+                    const currentItems = this.listTemplate.data.items;
+                    const newItems = await this.pokemonService.getPokemons({ offset: this.addOffset(), limit: this.limit });
+                    this.listTemplate.data.items = { ...currentItems, ...newItems };
+                    this.listTemplate.update();
+                    this.loading = false;
+                }
             });
         });
     }
@@ -36,16 +51,15 @@ class Main {
         let items = null;
 
         try {
-            items = await this.pokemonService.getPokemons({ offset: 0, limit: 20 });
+            items = await this.pokemonService.getPokemons({ offset: this.offset, limit: this.limit });
         }catch(e){
             console.error(e);
         }finally {
             if(items){
-                const listTemplate = new PokemonListTemplate({ items });
-                const detailsTemplate = new PokemonDetailsTemplate({ show: false });
-                this.addEvents(listTemplate, detailsTemplate);
-                listTemplate.mount("#list");
-                detailsTemplate.mount("#details");
+                this.listTemplate.data.items = items;
+                this.addEvents();
+                this.listTemplate.mount("#list");
+                this.detailsTemplate.mount("#details");
             }else{
                 console.info("it wasn't possible to load the pokemon data, please check your connection settings!");
             }
