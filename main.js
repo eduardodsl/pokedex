f9c009647de4d56f7baa81a8df505379e58c6ca4
Eduardo Augusto <eduardodsl@gmail.com>
2022-10-25 21:51:43 +0000

    adding loading events

--- main.js
+++ main.js
@@ -14,33 +14,65 @@ class Main {
         this.loading = false;
     }
 
+    /**
+     * Toggles the page's loading status
+     * @param {bool} isLoading 
+     */
+    setLoading(isLoading){
+        if(isLoading)document.body.classList.add("loading");
+        else document.body.classList.remove("loading");
+        this.loading = isLoading;
+    }
+
+    /**
+     * Increments the pagination offset
+     * @returns {number}
+     */
     addOffset(){
         return this.offset += 20;
     }
 
+    /**
+     * Adds events to the templates
+     */
     addEvents(){
+
+        // when the pokemon details template is loaded/updated, adds an event listener to the close button
+        this.detailsTemplate.onMount((data) => {
+            if(data.show){
+                document.querySelector("#close-details").addEventListener("click", (e) => {
+                    data.show = false;
+                    this.detailsTemplate.update();
+                });
+            }
+        });
+
+        // when the pokemon list template is loaded/updated, adds scrolling and item click events
         this.listTemplate.onMount((data) => {
             document.querySelectorAll("#pokemon-list li.pokemon-item").forEach((el) => {
-                el.addEventListener("click", (ev) => {
+                el.addEventListener("click", async (ev) => {
                     ev.stopPropagation();
-                    this.pokemonService.selectPokemon(ev.currentTarget.dataset.pkmid);
+                    const pokemon = this.pokemonService.selectPokemon(ev.currentTarget.dataset.pkmid);
+                    await this.pokemonService.getPokemonSpecies(pokemon);
                     this.detailsTemplate.data.pokemon = this.pokemonService.getSelectedPokemon();
                     this.detailsTemplate.data.show = true;
                     this.detailsTemplate.update();
                 });
             });
+
             document.querySelector("#search").addEventListener("scroll", async (e) => {
                 const scrollStatus = new ScrollStatus(e.currentTarget);
                 if(scrollStatus.isBottom() && !this.loading && !this.pokemonService.atLimit()){
-                    this.loading = true;
+                    this.setLoading(true);
                     const currentItems = this.listTemplate.data.items;
                     const newItems = await this.pokemonService.getPokemons({ offset: this.addOffset(), limit: this.limit });
                     this.listTemplate.data.items = { ...currentItems, ...newItems };
                     this.listTemplate.update();
-                    this.loading = false;
+                    this.setLoading(false);
                 }
             });
         });
+
     }
 
     /**
